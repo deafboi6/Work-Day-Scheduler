f9290c76fcf29d905e37c4656d9ecaed0b58b5e6
Jayden Chapman <jaychapmancode@gmail.com>
2022-10-31 15:31:44 -0700

    finished functionality

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,31 @@
-var dayTime = moment().format("MM/D/YYYY");
-var hourTime = moment().format("hh:mm A");
+var dayTime = moment().format("dddd MM/D/YYYY");
+var jumboHour = moment().format("hh:mm A");
 var docDay = document.getElementById("currentDay");
 var docTime = document.getElementById("currentTime");
+var hrTxt = document.getElementById("texthr1");
+var saveBtn1 = document.getElementById("saveBtn1");
 
 docDay.textContent = dayTime;
-docTime.textContent = hourTime;
+docTime.textContent = jumboHour;
 
 function pastPresentFuture() {
-    var background = document.getElementById("week");
+    var hourTime = moment().hour();
 
-    if (hourTime >= x) {
-        var week = document.getElementById("week");
+    for (let i = 9; i < 18; i++) {
+        if (i === hourTime) {
+            document.getElementById("texthr" + i).classList.add("present");
+        } else if (i > hourTime) {
+            document.getElementById("texthr" + i).classList.add("future");  
+        } else if (i < hourTime) {
+            document.getElementById("texthr" + i).classList.add("past"); 
+        }
+        document.getElementById(i).addEventListener("click", function() {
+            var id = this.getAttribute("id");
+            var text = document.getElementById("texthr" + id).value;
+            var storage = localStorage.setItem(i, text);
+        });
+        document.getElementById("texthr" + i).textContent = localStorage.getItem(i);
+    }
+}
 
-        week.style.background = "yellow";
-}
-}
+pastPresentFuture();
